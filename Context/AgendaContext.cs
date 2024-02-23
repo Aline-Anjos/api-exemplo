c7b4220ed3a0ed9ba13bd862b386a1dd75748aed
Aline Anjos <aline.aanjosx@gmail.com>
2024-02-23 00:34:19 -0300

    Add project files.

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,16 @@
+﻿
+using APIExemplo.Contatos;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIExemplo.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Contato> Contatos { get; set; }  
+    }
+}
---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIExemplo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora() 
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortTimeString()
+            };
+
+            return Ok(obj);
+        }
+        [HttpGet("Apresentar/{nome}")]
+        public IActionResult Apresentar(string nome)
+        {
+            var mensagem = $"Olá {nome}, seja bem-vindo!";
+            return Ok(new { mensagem });
+
+        }
+    }
+}
